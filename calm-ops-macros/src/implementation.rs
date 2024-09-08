16a2946caae897b7916b01c4a53cdc80046765f5
SLUCHABLUB <81807027+SLUCHABLUB@users.noreply.github.com>
2024-09-08 16:51:13 +0200

    Added macro implementation

---
+++ calm-ops-macros/src/implementation.rs
@@ -0,0 +1,95 @@
+use anyhow::{anyhow, bail, ensure, Result};
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::spanned::Spanned;
+use syn::{BinOp, Error, Expr, ExprBinary, ExprCast, ExprUnary, UnOp};
+
+fn trait_method_suffix(operation: &BinOp) -> Option<(&'static str, &'static str)> {
+    Some(match operation {
+        BinOp::Add(_) => ("Add", "add"),
+        BinOp::Sub(_) => ("Sub", "sub"),
+        BinOp::Mul(_) => ("Mul", "mul"),
+        BinOp::Div(_) => ("Div", "div"),
+        BinOp::Rem(_) => ("Rem", "rem"),
+        BinOp::BitXor(_) => ("BitXor", "bitor"),
+        BinOp::BitAnd(_) => ("BitAnd", "bitand"),
+        BinOp::BitOr(_) => ("BitOr", "bitor"),
+        BinOp::Shl(_) => ("Shl", "shl"),
+        BinOp::Shr(_) => ("Shr", "shr"),
+
+        _ => return None,
+    })
+}
+
+const UNSUPPORTED_OPERATION: &str = "Unsupported operation";
+
+pub(crate) fn implementation(
+    expression: Expr,
+    trait_prefix: &str,
+    method_prefix: &str,
+    supported_trait_suffixes: &[&str],
+) -> TokenStream {
+    let expression = convert_expression(
+        &expression,
+        trait_prefix,
+        method_prefix,
+        supported_trait_suffixes,
+    )
+    .unwrap_or_else(|error| Error::new(expression.span(), error).to_compile_error());
+
+    quote!({
+        extern crate self as calm_ops;
+        #expression
+    })
+}
+
+fn convert_expression(
+    expression: &Expr,
+    trait_prefix: &str,
+    method_prefix: &str,
+    supported_trait_suffixes: &[&str],
+) -> Result<TokenStream> {
+    // TODO: recurse
+    let arguments: Vec<&Expr>;
+
+    let trait_suffix;
+    let method_suffix;
+
+    let generic;
+
+    match expression {
+        Expr::Binary(ExprBinary {
+            left, op, right, ..
+        }) => {
+            arguments = vec![left, right];
+            (trait_suffix, method_suffix) =
+                trait_method_suffix(op).ok_or(anyhow!(UNSUPPORTED_OPERATION))?;
+            generic = None;
+        }
+        Expr::Cast(ExprCast { expr, ty, .. }) => {
+            arguments = vec![expr];
+            trait_suffix = "Cast";
+            method_suffix = "cast";
+            generic = Some(ty);
+        }
+        Expr::Unary(ExprUnary { op, expr, .. }) => {
+            arguments = vec![expr];
+            ensure!(matches!(op, UnOp::Neg(_)), UNSUPPORTED_OPERATION);
+            trait_suffix = "Neg";
+            method_suffix = "neg";
+            generic = None;
+        }
+        _ => bail!(UNSUPPORTED_OPERATION),
+    };
+
+    if !supported_trait_suffixes.contains(&trait_suffix) {
+        bail!("Unsupported operation")
+    }
+
+    let trait_name = format!("{trait_prefix}{trait_suffix}");
+    let method_name = format!("{method_prefix}_{method_suffix}");
+
+    Ok(quote! {
+        calm_ops::#trait_name::<#(#generic)?>::#method_name(#(#arguments),*)
+    })
+}
--- calm-ops-macros/src/lib.rs
+++ calm-ops-macros/src/lib.rs
@@ -1,28 +1,56 @@
 #![deny(clippy::all)]
 
+use crate::implementation::implementation;
 use proc_macro::TokenStream;
-use syn::parse_macro_input;
-use crate::wrapping::wrapping_implementation;
+use syn::{parse_macro_input, Expr};
 
-mod wrapping;
-mod util;
+mod implementation;
+
+const CHECKED_TRAIT_SUFFIXES: &[&str] = &["Add", "Div", "Mul", "Neg", "Rem", "Shl", "Shr", "Sub"];
+const ENCAPSULATING_TRAIT_SUFFIXES: &[&str] = &["Add", "Mul", "Neg", "Sub"];
+const SATURATING_TRAIT_SUFFIXES: &[&str] = &["Add", "Mul", "Sub"];
+const WRAPPING_TRAIT_SUFFIXES: &[&str] = &["Add", "Mul", "Neg", "Shl", "Shr", "Sub"];
 
 #[proc_macro]
-pub fn checked(_input: TokenStream) -> TokenStream {
-    todo!()
+pub fn checked(input: TokenStream) -> TokenStream {
+    implementation(
+        parse_macro_input!(input as Expr),
+        "Checked",
+        "checked",
+        CHECKED_TRAIT_SUFFIXES,
+    )
+    .into()
 }
 
 #[proc_macro]
-pub fn encapsulating(_input: TokenStream) -> TokenStream {
-    todo!()
+pub fn encapsulating(input: TokenStream) -> TokenStream {
+    implementation(
+        parse_macro_input!(input as Expr),
+        "Encapsulating",
+        "encapsulating",
+        ENCAPSULATING_TRAIT_SUFFIXES,
+    )
+    .into()
 }
 
 #[proc_macro]
-pub fn saturating(_input: TokenStream) -> TokenStream {
-    todo!()
+pub fn saturating(input: TokenStream) -> TokenStream {
+    implementation(
+        parse_macro_input!(input as Expr),
+        "Saturating",
+        "saturating",
+        SATURATING_TRAIT_SUFFIXES,
+    )
+    .into()
 }
 
 #[proc_macro]
 pub fn wrapping(input: TokenStream) -> TokenStream {
-    wrapping_implementation(parse_macro_input!(input as _)).into()
+    implementation(
+        parse_macro_input!(input as Expr),
+        "Wrapping",
+        "wrapping",
+        WRAPPING_TRAIT_SUFFIXES,
+    )
+    .into()
 }
